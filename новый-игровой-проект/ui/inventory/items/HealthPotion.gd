64db4ae76f5cd2fda01ca42d1018c71a06a99ec9
Weh2 <wehzxcgg@gmail.com>
2025-06-22 22:30:27 +0400

    34

--- новый-игровой-проект/ui/inventory/items/HealthPotion.gd
+++ новый-игровой-проект/ui/inventory/items/HealthPotion.gd
@@ -8,6 +8,8 @@ func _on_interactable_area_body_entered(body: Node2D) -> void:
 	if body.has_mathod("Player"):
 		player = body
 		playercollect()
+		await get_tree().create_timer(0.1).timeout
+		self.queue_free()
 
 func playercollect():
 	player.collect(item)
