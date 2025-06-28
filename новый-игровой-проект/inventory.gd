a1ff6ab4266a0fa7b905c06f264d47231028b26b
Weh2 <wehzxcgg@gmail.com>
2025-06-28 14:49:57 +0400

    2

--- новый-игровой-проект/inventory.gd
+++ новый-игровой-проект/inventory.gd
@@ -2,18 +2,44 @@ extends Resource
 class_name Inv
 
 signal update
+signal hotbar_updated  # Новый сигнал
 
 @export var slots: Array[InvSlot]
+@export var hotbar_slots: Array[InvSlot] = []  # Добавляем хотбар
+@export var hotbar_size: int = 6  # Количество слотов хотбара
 
+func _init():
+	# Инициализация обычных слотов
+	slots = []
+	
+	# Инициализация хотбара
+	hotbar_slots.resize(hotbar_size)
+	for i in hotbar_size:
+		hotbar_slots[i] = InvSlot.new()
 
+# Обновлённый метод insert
 func insert(item: InvItem):
-	var itemslots = slots.filter(func(slot): return slot.item == item)
+	# Сначала пробуем добавить в хотбар
+	for slot in hotbar_slots:
+		if slot.item == item and item.stackable:
+			slot.amount += 1
+			hotbar_updated.emit()
+			return
+	
+	# Затем в основной инвентарь
+	var itemslots = slots.filter(func(s): return s.item == item)
 	if !itemslots.is_empty():
 		itemslots[0].amount += 1
 	else:
-		var emptyslots = slots.filter(func(slot): return slot.item == null)
+		var emptyslots = slots.filter(func(s): return s.item == null)
 		if !emptyslots.is_empty():
 			emptyslots[0].item = item
 			emptyslots[0].amount = 1
 	update.emit()
-	
+
+# Новый метод для хотбара
+func set_hotbar_slot(index: int, item: InvItem, amount: int = 1):
+	if index < hotbar_slots.size():
+		hotbar_slots[index].item = item
+		hotbar_slots[index].amount = amount
+		hotbar_updated.emit()
