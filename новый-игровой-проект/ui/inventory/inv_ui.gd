4d008e5b34a67b7fc2c13c646043d98b2150e455
Weh2 <wehzxcgg@gmail.com>
2025-06-29 19:16:56 +0400

    hgj

--- новый-игровой-проект/ui/inventory/inv_ui.gd
+++ новый-игровой-проект/ui/inventory/inv_ui.gd
@@ -1,21 +1,34 @@
 extends Control
 
-@onready var inv:Inv = preload("res://ui/inventory/playerinv.tres")
-@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+@onready var inv: Inv = preload("res://ui/inventory/playerinv.tres")
+@onready var main_slots: Array = $NinePatchRect/GridContainer.get_children()
+@onready var equipment_panel = $EquipmentPanel
+@onready var hotbar_ui = $HotbarUI
 
 var is_open = false
 
-
 func _ready():
-	inv.update.connect(update_slots)
-	update_slots()
+	# Инициализация
+	inv.update.connect(update_main_slots)
+	inv.equipment_updated.connect(update_equipment)
+	inv.hotbar_updated.connect(update_hotbar)
+	
+	# Скрываем при старте
+	equipment_panel.visible = false
+	hotbar_ui.visible = true
 	close()
 
-func update_slots():
-	for i in range(min(inv.slots.size(), slots.size())):
-		slots[i].update(inv.slots[i])
+func update_main_slots():
+	for i in range(min(inv.slots.size(), main_slots.size())):
+		main_slots[i].update(inv.slots[i])
 
+func update_equipment():
+	for slot in equipment_panel.get_children():
+		if slot is EquipmentSlot:
+			slot.update(inv.equipment_slots[slot.slot_type])
 
+func update_hotbar():
+	hotbar_ui.update_slots()
 
 func _process(delta):
 	if Input.is_action_just_pressed("inventory"):
@@ -26,10 +39,12 @@ func _process(delta):
 
 func open():
 	visible = true
+	equipment_panel.visible = true
 	is_open = true
 
 func close():
 	visible = false
+	equipment_panel.visible = false
 	is_open = false
 	
 	
